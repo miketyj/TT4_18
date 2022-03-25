5108c3c7509bfbfa58662b3e63120af45d44091b
Joel Chang <42378287+joelczk@users.noreply.github.com>
2022-03-25 11:27:06 +0800

    Delete firebase_setup.py

--- firebase_setup.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-# Use a service account
-cred = credentials.Certificate('C:/TT4_18/tt4-18-firebase-adminsdk-m9szg-9161768ff9.json')
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
