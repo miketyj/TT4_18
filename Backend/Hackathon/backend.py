2c9993e03415eef8a6e6af9934b97d58e9998c95
miketyj <54625353+miketyj@users.noreply.github.com>
2022-03-25 16:26:34 +0800

    Updated get_customer_loans

--- Backend/Hackathon/backend.py
+++ Backend/Hackathon/backend.py
@@ -11,13 +11,15 @@ api = Api(app)
 @app.route('/customer-loans/<customer_id>')
 
 def get_customer_loans(customer_id):
+    result = {}
     customer_loan = requests.get('https://firestore.googleapis.com/v1/projects/tt4-18/databases/(default)/documents/CustomerLoan/' + customer_id).content
     data = json.loads(customer_loan.decode('utf-8'))
     loan_id = data['fields']['loan_id']['stringValue']
     loan = requests.get('https://firestore.googleapis.com/v1/projects/tt4-18/databases/(default)/documents/Loan/' + loan_id).content
     data = json.loads(loan.decode('utf-8'))
     loan_amount = data['fields']['loan_amount']['stringValue']
-    return loan_amount
+    result['loan_amount'] = loan_amount
+    return result
 
 
 
